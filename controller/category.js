aa8ed980bfba46f3504929c2aa24b0ef16c4d19c
DevSergioGC <sergiocamachog12@gmail.com>
2023-11-15 22:04:51 -0400

    feat(category-view): implement category services
    Implement category view for users. The users can make a GET, POST, PUT & DELETE request for the category view without problem.

--- controller/category.js
+++ controller/category.js
@@ -1,5 +1,56 @@
-export const getAllCategories = () => {}
-export const getProductsInCategory = () => {}
-export const AddNewCategory = () => {}
-export const updateCategory = () => {}
-export const deleteCategory = () => {}
+import {
+  GetAllCategoriesService,
+  GetProductsInCategoryService,
+  AddNewCategoryService,
+  UpdateCategoryService,
+  DeleteCategoryService
+} from '../services/category.js'
+import {
+  ValidateCategory,
+  ValidateUpdateCategory
+} from '../utils/validation.js'
+
+export const GetAllCategories = async (req, res) => {
+  const categories = await GetAllCategoriesService()
+  res.status(200).send(categories)
+}
+export const GetProductsInCategory = async (req, res) => {
+  const { categoryId } = req.params
+  const products = await GetProductsInCategoryService(categoryId)
+  res.status(200).send(products)
+}
+export const AddNewCategory = async (req, res) => {
+  const { error } = ValidateCategory(req.body)
+
+  if (error) {
+    res.status(400).send(error.details[0].message)
+  }
+
+  const { name, description } = req.body
+  const newCategory = {
+    name,
+    description
+  }
+  const category = await AddNewCategoryService(newCategory)
+  res.status(201).send(category)
+}
+export const UpdateCategory = async (req, res) => {
+  const { error } = ValidateUpdateCategory(req.body)
+
+  if (error) {
+    res.status(400).send(error.details[0].message)
+  }
+  const { categoryId } = req.params
+  const { name, description } = req.body
+  const changes = {
+    name,
+    description
+  }
+  const category = await UpdateCategoryService(categoryId, changes)
+  res.status(200).send(category)
+}
+export const DeleteCategory = async (req, res) => {
+  const { categoryId } = req.params
+  const message = await DeleteCategoryService(categoryId)
+  res.status(200).send(message)
+}
---
+++ services/category.js
@@ -0,0 +1,43 @@
+import { Category } from '../model/category.js'
+import { Product } from '../model/product.js'
+
+export const GetAllCategoriesService = async () => {
+  return await Category.findAll()
+}
+export const GetProductsInCategoryService = async (categoryId) => {
+  const categoryFound = await Category.findByPk(categoryId)
+  if (!categoryFound) {
+    return { error: 'Category not found' }
+  }
+  const products = await Product.findAll({
+    where: { categoryId }
+  })
+  if (!products || products.length === 0) {
+    return { error: 'No products found in this category' }
+  }
+  return products
+}
+export const AddNewCategoryService = async (category) => {
+  const alreadyExist = await Category.findOne({
+    where: { name: category.name }
+  })
+  if (alreadyExist) {
+    return { error: 'Category already exist' }
+  }
+  return Category.create(category)
+}
+export const UpdateCategoryService = async (categoryId, changes) => {
+  const categoryFound = await Category.findByPk(categoryId)
+  if (!categoryFound) {
+    return { error: 'Category not found' }
+  }
+  return await categoryFound.update(changes)
+}
+export const DeleteCategoryService = async (categoryId) => {
+  const categoryFound = await Category.findByPk(categoryId)
+  if (!categoryFound) {
+    return { error: 'Category not found' }
+  }
+  await categoryFound.destroy()
+  return { message: 'Category deleted' }
+}
