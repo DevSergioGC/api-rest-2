88917a40bad1c6c702ee2ba243b3b816a1bbd2cf
DevSergioGC <sergiocamachog12@gmail.com>
2023-11-15 20:09:02 -0400

    feat(db-models): add models
    Create the db models of Product and Category

---
+++ model/category.js
@@ -0,0 +1,16 @@
+import { DataTypes } from 'sequelize'
+import { sequelize } from '../db.js'
+
+export const Category = sequelize.define('category', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    required: true
+  },
+  description: String
+})
